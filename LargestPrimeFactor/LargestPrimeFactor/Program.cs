d6fc5a1f7187fca7ed863752cdc71d2cac1d97eb
Sinisa <sinisa@hackandcraft.com>
2014-07-20 22:09:29 +0200

    primes with no guessing

--- LargestPrimeFactor/LargestPrimeFactor/Program.cs
+++ LargestPrimeFactor/LargestPrimeFactor/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace LargestPrimeFactor
 {
@@ -11,21 +9,37 @@ namespace LargestPrimeFactor
         public static Random rnd = new Random();
         static void Main(string[] args)
         {
-            var r = FindAllFactors(600851475143);
+            var watch = new Stopwatch();
+            watch.Start();
+            long n = (long)int.MaxValue * (long) int.MaxValue;
+
+            Console.WriteLine("Searching for " + n);
+            Console.WriteLine("Fast Method");
+            EasierMethod(n, new HashSet<long>());
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            Console.WriteLine("Slow method");
+            var r = FindAllFactors(n);
             foreach (var factor in r)
             {
                 Console.WriteLine(factor);
             }
-
+            
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
             Console.Read();
 
         }
 
         public static bool IsPrime(long n)
         {
+            if (n == 2)
+                return true;
             if (n%2 == 0)
                 return false;
-            for (int i = 2; i < Math.Ceiling(Math.Sqrt(n)); i++)
+            if (n%Math.Sqrt(n) == 0)
+                return false;
+            var limit = Math.Ceiling(Math.Sqrt(n)) + 1;
+            for (int i = 2; i < limit; i++)
             {
                 if (n%i == 0)
                     return false;
@@ -40,6 +54,7 @@ namespace LargestPrimeFactor
             {
                 var factor = FindFactor(n);
                 result.Add(factor);
+                
                 n = n/factor;
             }
             result.Add(n);
@@ -51,10 +66,12 @@ namespace LargestPrimeFactor
             long x;
             long y;
             long d = 1;
+            //int limit = int.MaxValue < n ? int.MaxValue : (int)n;
             while (d == 1)
             {
-                x = rnd.Next(1,(int) Math.Sqrt(n));
-                y = rnd.Next(1, (int)Math.Sqrt(n));
+                x = LongRandom(2,long.MaxValue, rnd);
+                y = LongRandom(2, long.MaxValue, rnd);
+ 
                 if(x!=y)
                     d = GCD(Math.Abs(x-y), n);
                 if (d != 1 && !IsPrime(d))
@@ -76,6 +93,46 @@ namespace LargestPrimeFactor
             {
                 return GCD(min, modulo);
             }
+        }
+
+        public static long LongRandom(long min, long max, Random rand)
+        {
+            byte[] buf = new byte[8];
+            rand.NextBytes(buf);
+            long longRand = BitConverter.ToInt64(buf, 0);
+
+            return (Math.Abs(longRand % (max - min)) + min);
+        }
+
+        public static void EasierMethod(long n, HashSet<long> set )
+        {
+            var limit = Math.Ceiling(Math.Sqrt(n)) + 1;
+            int prime=1;
+            if (set.Contains(n))
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                for (int i = 2; i < limit; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        Console.WriteLine(i);
+                        set.Add(i);
+                        prime = i;
+                        break;
+                    }
+                }
+            }
+            if (prime == 1)
+            {
+                if (!set.Contains(n))
+                    Console.WriteLine(n);
+            }
+            else
+                EasierMethod(n / prime, set);
+                
         }
     }
 }
